2caece49444cad2056d79aa1632c8a2f152eebd4
azril <azriljanuar661@gmail.com>
2024-10-31 21:36:09 +0700

    login1

--- app/src/main/java/com/example/bsecure/login/LoginScreen.kt
+++ app/src/main/java/com/example/bsecure/login/LoginScreen.kt
@@ -19,6 +19,7 @@ import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import com.example.bsecure.R
 
 @Preview
 @Composable
