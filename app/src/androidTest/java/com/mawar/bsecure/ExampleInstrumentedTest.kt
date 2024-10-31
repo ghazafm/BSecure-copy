5780bf6b3ef07371f60abd01f7332e25dc3f76c8
ghazafm <fauzan.ghaza@gmail.com>
2024-10-31 23:31:14 +0700

    edit structure

--- app/src/androidTest/java/com/mawar/bsecure/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/mawar/bsecure/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.example.bsecure", appContext.packageName)
+        assertEquals("com.mawar.bsecure", appContext.packageName)
     }
 }
